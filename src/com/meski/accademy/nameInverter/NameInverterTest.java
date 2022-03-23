f8578c45c17c30caec26fd68e633a12932154465
Mehmed Maljoki <mehmed.maljoki@porscheinformatik.com>
2022-03-23 12:30:19 +0100

    adding more tests: -postNominal and Honorific

--- src/com/meski/accademy/nameInverter/NameInverterTest.java
+++ src/com/meski/accademy/nameInverter/NameInverterTest.java
@@ -2,6 +2,10 @@ package com.meski.accademy.nameInverter;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 
 public class NameInverterTest {
@@ -39,21 +43,44 @@ public class NameInverterTest {
         assertInverted("   First  Last", "Last, First");
     }
 
+    @Test
+    public void ignoreHonorific() throws Exception {
+        assertInverted("Mr. First Last", "Last, First");
+        assertInverted("Mrs. First Last", "Last, First");
+    }
+
+    @Test
+    public void postNominals_stayAtEnd() throws Exception {
+        assertInverted("First Last Sr.", "Last, First Sr.");
+    }
+
     private String invertName(String name) {
         if (name == null || name.length() <= 0) {
             return "";
         } else {
-            String[] names = name.trim().split("\\s+");
-            if (names.length == 1) {
-                return names[0];
+            List<String> names = splitNames(name);
+            if (names.size() > 1 && isHonorific(names.get(0))) {
+                names.remove(0);
+            }
+            if (names.size() == 1) {
+                return names.get(0);
             } else {
-                return String.format("%s, %s", names[1], names[0]);
+                String postNominal = "";
+                if (names.size() > 2) {
+                    postNominal = names.get(2);
+                }
+                return String.format("%s, %s %s", names.get(1), names.get(0), postNominal).trim();
             }
         }
     }
 
+    private boolean isHonorific(String word) {
+        return word.matches("Mr\\.|Mrs\\.");
+    }
 
-
+    private ArrayList splitNames(String name) {
+        return new ArrayList(Arrays.asList(name.trim().split("\\s+")));
+    }
 
 
 }
